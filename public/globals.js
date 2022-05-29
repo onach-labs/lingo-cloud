22c0557071b8da9cdad8388e26cdbe9efa02c0a3
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2022-05-29 16:48:05 -0400

    Add files via upload

--- public/globals.js
+++ public/globals.js
@@ -1,3 +1,25 @@
-const version = "Beta 0.1 - \"Closet\""
+const version = "Beta 0.2 - \"Welcomer\"";
 
-const pride_ver = "0.0.1"
+const pride_ver = "0.0.1";
+
+const quotes = [
+    `Lana: It was in law school. I was in my third year, and [Mia] was auditing the class. She was different than the other students.\r\n
+    Phoenix: Different?\r\n
+    Lana: She was strong... She'd do anything to become a defense attorney. Anything. That... was probably why she was attracted to me.\r\n
+    Phoenix: E-excuse me!?\r\n
+    Ema: Intellectually attracted! Lana was top of her class in school.\r\n
+    Lana: I was the best there was.\r\n
+    Phoenix: Oh...\r\n
+    Ema: I'm doing pretty good in school too, by the way!\r\n
+    Phoenix: (It sounds a bit different when Ema says it...)\r\n
+    \u0009 from "Rise from the Ashes" - Phoenix Wright: Ace Attorney\r\n
+    `, `
+    Theo: Would you mind talking about what depression is like?\r\n
+    Madeline: ... It sucks.\r\n
+    \u0009 from "Reflection" - Celeste\r\n
+    `, `
+    Theo: This isn't canon isn't it?\r\n
+    \u0009 from "Mirror Temple" (B-Side) - Celeste\r\n
+    `
+]
+
